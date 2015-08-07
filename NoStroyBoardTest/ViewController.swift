c46cfc87f8eb0d028a8ccd839dce7f1ae32307f0
Hao Xin <haoxin@offercal.com>
2015-08-07 16:46:48 +0800

    Initial Commit

---
+++ NoStroyBoardTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoStroyBoardTest
+//
+//  Created by Hao Xin on 15/8/7.
+//  Copyright (c) 2015年 Hao Xin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
